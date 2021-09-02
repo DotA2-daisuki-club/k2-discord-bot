368bfe06822de1b258521f18f1f8bebe5541257c
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2021-09-02 15:29:09 +0900

    gazouボットとりあえずできた

--- src/gazou.ts
+++ src/gazou.ts
@@ -1,12 +1,22 @@
 import { Message, TextChannel } from 'discord.js'
 import { GAZOU_CHANNEL_ID } from './const'
 
-const responceGazou = async (message:Message): Promise<void> => {
+const responseGazou = async (message: Message): Promise<void> => {
   const client = message.channel.client
   const gazouChannel = await client.channels.fetch(GAZOU_CHANNEL_ID)
   if (gazouChannel.isText() && gazouChannel instanceof TextChannel) {
-    console.log(gazouChannel)
+    const messages = await gazouChannel.messages.fetch()
+    const images = messages
+      .filter(message => Object.keys(message.attachments).length === 0)
+      .map(message => message.attachments.map(image => image.attachment))
+      .flat(1)
+
+    const randomImage = images[Math.floor(Math.random() * images.length)]
+
+    if (typeof randomImage === `string`) {
+      await message.channel.send(randomImage)
+    }
   }
 }
 
-export default responceGazou
+export default responseGazou
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
+import { Client } from 'discord.js'
+import responseGazou from './gazou'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config()
 
-import { Client } from 'discord.js'
-import responceGazou from './gazou'
-
-const main = () => {
+const main = (): void => {
   const client = new Client()
 
   client.once(`ready`, async () => {
     console.log(`準備完了`)
   })
-  
+
   client.on(`message`, async message => {
     if (message.content === `/gazou`) {
-      responceGazou(message)
+      await responseGazou(message)
     }
   })
-  
+
   client.login(process.env.DISCORD_BOT_TOKEN)
 }
 
