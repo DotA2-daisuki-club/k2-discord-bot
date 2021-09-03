4a0b2d16a61eadfb85f1c6e45dd92cbbe152e1a6
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2021-09-03 12:57:58 +0900

    雑に連投対策

--- src/gazou.ts
+++ src/gazou.ts
@@ -26,20 +26,42 @@ const responseGazou = async (
   }
 
   if (gazouChannel.isText() && gazouChannel instanceof TextChannel) {
-    const messages = await gazouChannel.messages.fetch()
+    const messages = await gazouChannel.messages.fetch({ limit: 100 })
     const images = messages
       .filter(message => Object.keys(message.attachments).length === 0)
       .map(message =>
         message.attachments.map(image => ({
           url: image.attachment,
+          filename: image.name,
           author: message.author.username
         }))
       )
       .flat(1)
 
-    const randomImage = images[Math.floor(Math.random() * images.length)]
+    // ファイル名と投稿者が被ってたら重複とみなし削除
+    let imageCache: Omit<typeof images[number], 'url'>[] = []
+    let imagesNotDuplicate: typeof images = []
+    for (const image of images) {
+      if (
+        !imageCache.some(
+          ({ filename, author }) =>
+            filename === image.filename && author === image.author
+        )
+      ) {
+        imageCache = [
+          ...imageCache,
+          { filename: image.filename, author: image.author }
+        ]
+        imagesNotDuplicate = [...imagesNotDuplicate, image]
+      }
+    }
+
+    const randomImage =
+      imagesNotDuplicate[Math.floor(Math.random() * imagesNotDuplicate.length)]
 
     if (typeof randomImage.url === `string`) {
+      console.log(images.length)
+      console.log(imagesNotDuplicate.length)
       message.channel.send(randomImage.url)
       message.channel.send(`by ${randomImage.author}`)
     }
